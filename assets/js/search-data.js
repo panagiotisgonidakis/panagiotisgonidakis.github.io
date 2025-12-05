87384680cfdecc0d0a12702d71717a913464f674
panagiotisgonidakis <panosgon@gmail.com>
2025-12-05 13:58:00 +0000

    Deploying to gh-pages from @ panagiotisgonidakis/panagiotisgonidakis.github.io@8b691df908bb1302d53b6c88fe392ced04560723 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -508,14 +508,28 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%70%61%6E%61%67%69%6F%74%69%73.%67%6F%6E%69%64%61%6B%69%73@%6B%75%6C%65%75%76%65%6E.%62%65", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://www.linkedin.com/in/panagiotis-gonidakis", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0000-0001-5797-0794", "_blank");
+        },
+      },{
+        id: 'social-researchgate',
+        title: 'ResearchGate',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.researchgate.net/profile//", "_blank");
         },
       },{
         id: 'social-rss',
@@ -529,14 +543,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=-9mbCrUAAAAJ&hl=en", "_blank");
         },
       },{
       id: 'light-theme',
