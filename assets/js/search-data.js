94db754a477e6e4f3c25b3acc6e46c7d5e496ff4
panagiotisgonidakis <panosgon@gmail.com>
2026-06-30 13:11:25 +0000

    Deploying to gh-pages from @ panagiotisgonidakis/panagiotisgonidakis.github.io@80d92ef8c81126c670c8ea0c61c0cec5dddd614b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -24,7 +24,7 @@ ninja.data = [{
             window.location.href = "/cv/";
           },
         },{id: "nav-news",
-          title: "News",
+          title: "news",
           description: "",
           section: "Navigation",
           handler: () => {
