cded49246658b0f955cc7f19ccdbc72a769740ad
panagiotisgonidakis <panosgon@gmail.com>
2026-06-30 13:00:51 +0000

    Deploying to gh-pages from @ panagiotisgonidakis/panagiotisgonidakis.github.io@629a7e6b255a9bbc59cbcd4df77c7668a11713d9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
+        },{id: "nav-news",
+          title: "News",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/news/";
+          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
